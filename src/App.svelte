2d4156fd0ef32c47dd3bced781fdcd51a3290750
Rohit Patil <rahulhimesh09@gmail.com>
2021-09-03 00:24:22 +0530

    [ADD] last commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let todos = [];
+  let inp;
+  const add = () => (todos = [...todos, inp]);
+  const remove = (ind) => (todos = todos.filter((v, i) => i !== ind));
+</script>
+
+<input bind:value={inp} type="text" />
+<button on:click={add}>Add</button>
+{#each todos as todo, index (index)}
+  <li>{todo} <button on:click={remove(index)}>🚮</button></li>
+{/each}
